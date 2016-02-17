260f29e4eb03ff9f2b9aea79b0483ab0e6b3339e
Nate Bazar <Nate Bazar>
2016-02-16 21:07:22 -0700

    initial commit

--- MVC_WebProject/Controllers/HelloWorldController.cs
+++ MVC_WebProject/Controllers/HelloWorldController.cs
@@ -1,17 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
 using System.Web.Mvc;
 
-namespace MVC_WebProject.Controllers
+namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
-        // GET: HelloWorld
+        // 
+        // GET: /HelloWorld/ 
+
+        //public string Index()
+        //{
+        //    return "This is my <b>default</b> action..."; //Returns single string if index page is accessed
+        //}
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome(string name, int Age = 0)
+        {
+            if (Age != 0)
+            {
+                return HttpUtility.HtmlEncode("Hello " + name + ", You are: " + Age + "years old...");
+            } else
+            {
+                return HttpUtility.HtmlEncode("Hello " + name + ", You never told me how old you are...");
+            }
         }
     }
 }
