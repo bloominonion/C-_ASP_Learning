78c9ef9c5ac3c4bbedeb1692b036d9534d2733ce
Nate Bazar <Nate Bazar>
2016-02-16 21:03:45 -0700

    Initial commit

---
+++ MVC_WebProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_WebProject.Startup))]
+namespace MVC_WebProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
