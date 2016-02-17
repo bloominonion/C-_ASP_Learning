005650c538bca44c93f8e39769d34a5949cd1d87
Nate Bazar <Nate Bazar>
2016-02-17 16:57:26 -0700

    Dinner break...stupic local DB

--- MVC_WebProject/Models/Movie.cs
+++ MVC_WebProject/Models/Movie.cs
@@ -6,14 +6,14 @@ namespace MVC_WebProject.Models
     public class Movie
     {
         public int ID { get; set; }
-        public string Title { get; set;}
+        public string Title { get; set; }
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
-        public decimal  Price { get; set; }
+        public decimal Price { get; set; }
     }
 
-    public class MovieDBContext : DbContext 
+    public class MovieDBContext : DbContext
     {
-
+        public DbSet<Movie> Movies { get; set; }
     }
 }
